dbdbb38c24f5e43cfdd1ab46b5ea5d531599cca1
Sandilandssa <sandile.gitf8@gmail.com>
2023-08-04 11:37:57 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,9 +4,9 @@
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
- * Return: converted number
+ * Return: the converted number
  */
-unsigned int binary_to_uint(const char *c)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
@@ -16,9 +16,9 @@ unsigned int binary_to_uint(const char *c)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (c[i] < '0' || b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (c[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);
